f3f994e1749061a89fbb2a8f12d9c219faeb42ab
priscila2.santos <priscila2.santos@alunos.ifsuldeminas.edu.br>
2025-07-07 19:30:39 +0000

    ns

---
+++ lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+// import 'presentation/pages/stack/home_page3.dart';
+import 'presentation/pages/text/horaDePraticar1/1.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: HomePage(),
+    );
+  }
+}
